ace7f6a33fa12db02518627696bd414f8f60f016
Нашко Анастасия Николаевна <nashko.an@kd.local>
2023-03-04 16:27:58 +0300

    New methods in main page

--- base_page.py
+++ base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
 
     def __init__(self, browser, url):
@@ -6,3 +8,15 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10): #вспомогательный метод поиска элемента в нашей базовой странице BasePage, который будет возвращать нам True или False
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what): #is_element_present, в котором будем перехватывать исключение. В него будем передавать два аргумента: как искать (css, id, xpath и тд) и собственно что искать (строку-селектор)
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
