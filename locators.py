9c3f80b9ef75a7aaf0b443dc7e07ba39916c010d
Нашко Анастасия Николаевна <nashko.an@kd.local>
2023-03-04 18:47:39 +0300

    LoginPage

---
+++ locators.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    def register_new_user(self):
+        self.browser.find_element(By.CSS_SELECTOR, "registration-email").send_keys("1234@mail.ru")
+        self.browser.find_element(By.CSS_SELECTOR, "registration-password1").send_keys("name123")
+        self.browser.find_element(By.CSS_SELECTOR, "registration-password2").send_keys("name123")
+        self.browser.find_element(By.CSS_SELECTOR, "registration-submit").click()
+
+    def go_to_login_page(self):
+        self.browser.find_element(By.CSS_SELECTOR, "login-username").send_keys("1234@mail.ru")
+        self.browser.find_element(By.CSS_SELECTOR, "login-password").send_keys("name123")
+        self.browser.find_element(By.CSS_SELECTOR, "login_submit").click()
